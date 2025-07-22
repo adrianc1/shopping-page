5e7de996c0bb9c82f07ed27734f31f69cf679182
adriansito <adriancrosby02@gmail.com>
2025-07-21 19:13:57 -0700

    installed userEvent from testing-lib updated test folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
-const App = () => <h1>First Test Heres!</h1>;
+import { useState } from 'react';
+
+const App = () => {
+	const [heading, setHeading] = useState('Magnificent Monkeys');
+
+	const clickHandler = () => {
+		setHeading('Radical Rhinos');
+	};
+
+	return (
+		<>
+			<button type="button" onClick={clickHandler}>
+				Click Me
+			</button>
+			<h1>{heading}</h1>
+		</>
+	);
+};
 
 export default App;
