5a96912dc534ca45841a9ba4eb85cb96d41c871f
adriansito <adriancrosby02@gmail.com>
2025-07-28 23:52:45 -0700

    created product cards on shopping page displaying title, price, and rating

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,17 @@
 import products from '../data/mockData';
-import testimg from '../assets/testimg.jpg';
 const Card = () => {
 	const items = products;
 
 	return (
-		<ul className="grid grid-cols-5 gap-3 w-full">
+		<ul className="grid grid-cols-4 gap-4 w-full h-[200px] ">
 			{items.map((item) => {
 				return (
-					<li className="border-2">
-						<div className="prd-img-cnt">
-							<img src={item.image} alt="" />
+					<li className="border-2 rounded-2xl flex flex-col ">
+						<div className="prd-img-cnt flex-3 flex items-center  ">
+							<img src={item.image} alt="" className="w-full object-cover  " />
 						</div>
-						<div className="prd-info flex flex-col">
-							<span>{item.title}</span>
+						<div className="prd-info flex flex-col flex-1 py-12">
+							<span className="truncate">{item.title}</span>
 							<span>{item.price}</span>
 							<span>{item.rating.rate}</span>
 						</div>
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -2,7 +2,7 @@ import BackHomeBtn from '../components/BackHomeBtn';
 import Card from '../components/Card';
 const Shop = () => {
 	return (
-		<div className="flex flex-col">
+		<div className="flex flex-col min-h-full">
 			<BackHomeBtn />
 			<h1>The Shopping page!</h1>
 			<Card />
