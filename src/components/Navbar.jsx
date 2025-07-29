9ea8fe91b880323bc6fe1b0f05719bef9e3d7cf2
adriansito <adriancrosby02@gmail.com>
2025-07-28 18:59:24 -0700

    created links to home and shopping. Created tailwind component classes for primary text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import ViewCartBtn from './ViewCartBtn';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 	return (
-		<nav className="flex w-full items-center justify-between px-6 py-6">
+		<nav className="flex w-full items-center justify-between px-6 py-6 text-primary font-bold">
 			<span>logo</span>
 			<ul className="flex gap-4 items-center justify-center">
-				<li>Home</li>
-				<li>Shopping</li>
+				<Link to="/">
+					<li>Home</li>
+				</Link>
+				<Link to="shop">
+					<li>Shopping</li>
+				</Link>
 				<ViewCartBtn />
 			</ul>
 		</nav>
--- src/components/ViewCartBtn.jsx
+++ src/components/ViewCartBtn.jsx
@@ -5,7 +5,7 @@ const ViewCartBtn = () => {
 	return (
 		<Link
 			to="cart"
-			className="flex gap-2 px-2 py-1 text-white bg-green-700 rounded hover:bg-green-800 transition"
+			className="flex gap-2 px-2 py-2 text-white bg-green-700 rounded hover:bg-green-800 transition"
 		>
 			<ShoppingCart />
 			<span>0</span>
