22e782b228420432210eaf1655e6d3a19ada3bb8
adriansito <adriancrosby02@gmail.com>
2025-08-13 20:55:35 -0700

    display current cart item title and price in the cart page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import ViewCartBtn from './ViewCartBtn';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = ({ cart }) => {
-	console.log();
 	return (
 		<nav className="flex w-full items-center justify-between px-6 py-6 text-primary font-bold bg-[#fff3e0]">
 			<span>logo</span>
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,29 @@
 import BackHomeBtn from '../components/BackHomeBtn';
+import { useOutletContext } from 'react-router-dom';
 import { useState } from 'react';
 import BackToShopBtn from '../components/BackToShopBtn';
 const Cart = () => {
+	const { cart } = useOutletContext();
+
 	return (
 		<div className="w-full flex flex-col justify-start items-start border-2">
 			<BackToShopBtn />
-			<h1>Hey I'm a cart!!</h1>
+			<div className="mx-auto w-full ">
+				<h1 className="text-2xl">My Cart</h1>
+				<ul className="w-full">
+					{cart.map((item) => {
+						return (
+							<li key={item.id} className="flex border w-full justify-between">
+								<div className="flex">
+									<button className="text-red-500 font-bold px-4">X</button>
+									<div className="">{item.title}</div>
+								</div>
+								<div className="px-4">{item.price}</div>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 		</div>
 	);
 };
